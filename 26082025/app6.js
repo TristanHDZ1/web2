5ce58ccf7fb968d227906f33630c34a57705ed91
TristanHDZ1 <tristancruz68@aragon.unam.mx>
2025-08-26 10:24:23 -0600

    fix (Clase 26082025): Tarea

---
+++ 26082025/app6.js
@@ -0,0 +1,20 @@
+let numero1 = parseFloat(prompt("Ingresa el numero : "));
+let numero2 = parseFloat(prompt("Ingresa el otro numero : "));
+
+
+let resultadoSuma = numero1 + numero2;
+let resultadoResta = numero1 - numero2; 
+let resultadoMultiplicacion = numero1 * numero2;
+let resultadoDiv = numero1 / numero2;
+let resultadoModulo = numero1 % numero2;
+
+
+
+console.log(resultadoSuma, resultadoResta,resultadoMultiplicacion,resultadoDiv,resultadoModulo);
+
+console.log("------- Diferencia -------")
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(resultadoDiv);
+console.log(resultadoModulo);
