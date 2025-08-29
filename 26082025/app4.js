dd6ff0bf25186b5eadf0634e5f3bbb88b5cac0bc
Tristán Javier Cruz Hernández <124111638+TristanHDZ1@users.noreply.github.com>
2025-08-28 21:00:50 -0600

    Add files via upload

---
+++ 26082025/app4.js
@@ -0,0 +1,7 @@
+let variable1 = parseInt(prompt("Ingresa un numero :"));
+let variable2 = parseInt(prompt("Ingresa un numero :"));
+
+
+let resultado = (numero1) + (numero2);
+
+console.log(resultado);
